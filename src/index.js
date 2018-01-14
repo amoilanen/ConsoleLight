25db4266feba0a6b87a9b6737e33ea423671bf16
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-01-14 23:33:43 +0200

    Displayed object/array properties can be collapsed and expanded.

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ const OneLineSummaryDisplayedValue = ( { value, prefix }) => {
     formattedValue = value;
   }
 
-  return (<div class="short-displayed-value">
+  return (<div class="displayed-value-one-line-summary">
     <span>{prefix ? `${prefix}: ` : ''}</span>
     <pre>{formattedValue}</pre>
   </div>);
@@ -65,34 +65,54 @@ const DisplayedValue = ({ value, prefix }) => {
     return <OneLineSummaryDisplayedValue value={value} prefix={prefix} />;
   }
 
+  const state = {
+    expandedState: 'collapsed'
+  };
+
+  const toggle = event => {
+    if (state.expandedState === 'expanded') {
+      state.expandedState = 'collapsed';
+    } else {
+      state.expandedState = 'expanded';
+    }
+
+    const { expandedState } = state;
+
+    const containerElement = event.currentTarget;
+    containerElement.setAttribute('class', `${expandedState}-displayed-value`);
+
+    const iconElement = containerElement.querySelector('.displayed-value-icon');
+    iconElement.setAttribute('class', `${expandedState}-displayed-value-icon displayed-value-icon`);
+    const expandedStateIcon = expandedState === 'expanded' ? EXPANDED_ICON : COLLAPSED_ICON;
+    iconElement.innerHTML = expandedStateIcon;
+
+    const childrenContainer = containerElement.querySelector('.displayed-value-children');
+    childrenContainer.setAttribute('class', `${expandedState}-displayed-value-children displayed-value-children`);
+
+    event.stopPropagation();
+  };
+
   const COLLAPSED_ICON = '\u25B6';
   const EXPANDED_ICON = '\u25BC';
 
-  const state = {
-    isExpanded: true
-  };
+  const expandedState = state.expandedState;
+  const expandedStateIcon = expandedState === 'expanded' ? EXPANDED_ICON : COLLAPSED_ICON;
 
-  //TODO: Make interactive, can be collapsed/expanded by clicking the short value or icon
-  if (!state.isExpanded) {
-    return (<div class="collapsed-displayed-value">
-      <span class="collapsed-displayed-value-icon">{COLLAPSED_ICON}</span>
-      <OneLineSummaryDisplayedValue value={value} prefix={prefix} />
-    </div>);
-  } else {
-    const displayedProperties = Object.keys(value).map(propertyName => {
-      const propertyValue = value[propertyName];
+  const childrenValues = Object.keys(value).map(propertyName => {
+    const propertyValue = value[propertyName];
 
-      return <div class="child-displayed-value">
-        <DisplayedValue value={propertyValue} prefix={propertyName} />
-      </div>;
-    });
+    return <div class="child-displayed-value">
+      <DisplayedValue value={propertyValue} prefix={propertyName} />
+    </div>;
+  });
 
-    return (<div class="expanded-displayed-value">
-      <span class="expanded-displayed-value-icon">{EXPANDED_ICON}</span>
-      <OneLineSummaryDisplayedValue value={value} prefix={prefix} />
-      {displayedProperties}
-    </div>);
-  }
+  return (<div class={`${expandedState}-displayed-value`} onclick={toggle}>
+    <span class={`${expandedState}-displayed-value-icon displayed-value-icon`}>{expandedStateIcon}</span>
+    <OneLineSummaryDisplayedValue value={value} prefix={prefix} />
+    <div class={`${expandedState}-displayed-value-children displayed-value-children`}>
+      {childrenValues}
+    </div>
+  </div>);
 };
 
 const Output = ({ evaluationResults }) => {
